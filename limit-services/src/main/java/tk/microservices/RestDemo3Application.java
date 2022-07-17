44f09510c065ce6a9363cbc292b7c30d993169fe
Administrator <Administrator@host.docker.internal>
2022-07-18 04:39:38 +0530

    microservice version 1 commit

---
+++ limit-services/src/main/java/tk/microservices/RestDemo3Application.java
@@ -0,0 +1,13 @@
+package tk.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemo3Application.class, args);
+	}
+
+}
---
+++ limit-services/src/test/java/tk/microservices/RestDemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
