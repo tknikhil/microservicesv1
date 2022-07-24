7ee0cb5f4f2683bf68b559512cf0479396e28c8e
nikhil tk <nikhiltk02@gmail.com>
2022-07-24 06:48:30 +0530

    added link to spring-cloud-config-server in bootstrap.properties so that access the resource from git-localconfig-repo2 added  spring-cloud-strater-bootstrap in limit-service

--- limit-services/src/test/java/tk/microservices/LimitServiceApplicationTests.java
+++ limit-services/src/test/java/tk/microservices/LimitServiceApplicationTests.java
@@ -3,7 +3,7 @@ package tk.microservices;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.cloud.config.import-check.enabled=false"})
 class LimitServiceApplicationTests {
 
 	@Test
