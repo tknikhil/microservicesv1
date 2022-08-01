97043461cc3285f7166e3057253a88ae25341a30
nikhil tk <nikhiltk02@gmail.com>
2022-08-01 05:36:29 +0530

    Adding repo in micro services to collect data from database

--- currency-exchange-services/src/main/java/tk/microservices/controller/CurrencyExchangeController.java
+++ currency-exchange-services/src/main/java/tk/microservices/controller/CurrencyExchangeController.java
@@ -8,20 +8,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import tk.microservices.dao.ExchangeServiceRepo;
 import tk.microservices.entity.ExchangeValue;
 
 @RestController
 public class CurrencyExchangeController {
 	@Autowired
 	private Environment environment;
+	@Autowired
+	private ExchangeServiceRepo exchangeServieceRepo;
+
+//	@GetMapping("currency-exchange/from/{from}/to/{to}")
+//	public ExchangeValue retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
+//		// HardCode ExchangeValue exchangeValue = new
+//		ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+//		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+//		return exchangeValue;
+//
+//	}
 	
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
-	public ExchangeValue retriveExchangeValue(@PathVariable String from,@PathVariable String to) {
-		//HardCode
-		ExchangeValue exchangeValue = new ExchangeValue(1000L,from,to,BigDecimal.valueOf(65));
+	public ExchangeValue retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		ExchangeValue exchangeValue =exchangeServieceRepo.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return exchangeValue;
-		
+
 	}
 
 }
